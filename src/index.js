fbf14860c0fb176f8d45f9041dde156c33cad0da
Alen Jo <alenjo874@gmail.com>
2022-01-18 09:23:44 -0500

    completed deliverable, still need to finish extra points

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const taskForm = document.querySelector("#create-task-form");
+const taskDescInput = document.querySelector("#new-task-description");
+const createNewTaskBtn = document.querySelector("#submitBtn");
+
+createNewTaskBtn.addEventListener("click", createNewTask);
+
+function createNewTask(event) {
+  event.preventDefault();
+  const myToDoList = document.querySelector("#list");
+  const liTag = document.createElement("li");
+  const btnTag = document.createElement("button");
+  btnTag.textContent = "X";
+  liTag.textContent = `${taskDescInput.value}  `;
+
+  myToDoList.appendChild(liTag);
+  liTag.append(btnTag);
+
+  taskForm.reset();
+
+  btnTag.addEventListener("click", removeListItemBtn);
+
+  function removeListItemBtn() {
+    liTag.remove();
+  }
+}
